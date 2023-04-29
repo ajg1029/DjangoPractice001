b35e013bfad8ff3fa23da6542ab742b61f5cda9f
ajg1029 <urban.seonbi.deon@gmail.com>
2023-04-30 03:59:54 +0900

    URL, Namespace

--- 04_review4/mypjt/urls.py
+++ 04_review4/mypjt/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for mypjt project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,13 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from articles import views
+from django.urls import path, include
+from app1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
-    path('throw/', views.throw),
-    path('catch/', views.catch, name='catch'),
-    path('hello/<str:name>/', views.hello)
+    path('app1/', views.index),
+    path('app1/', include('app1.urls')),
+    path('app2/', include('app2.urls'))
 ]
