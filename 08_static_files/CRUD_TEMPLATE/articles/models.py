6749289795b260ce80933a9f9cd0937cf3d94bda
ajg1029 <urban.seonbi.deon@gmail.com>
2023-05-07 00:12:41 +0900

    이미지 업로드 (create)

---
+++ 08_static_files/CRUD_TEMPLATE/articles/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    images = models.ImageField(blank=True, upload_to='images/')
+    # 주의사항 : 문자열 기반 필드에는 null=True 를 사용하지 않는 것이 좋다.
+    # blank 는 validation 단계에서 적용되고
+    # null 은 DB 에서 적용되는데
+    # 문자열 필드일 경우 둘 다 데이터 없음으로 처리되며
+    # django 는 이것을 '데이터 없음'의 상태를 표현하는 '중복'된 방법으로 본다.
+
+    def __str__(self):
+        return self.title
